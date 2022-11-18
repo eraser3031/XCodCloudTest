4e7a7fc50f758004db28289c661b0596c5c0330d
eraser <eraser3031@gmail.com>
2022-11-18 11:26:19 +0900

    Initial Commit

---
+++ XCodCloudTest/XCodCloudTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCodCloudTestApp.swift
+//  XCodCloudTest
+//
+//  Created by 김예훈 on 2022/11/18.
+//
+
+import SwiftUI
+
+@main
+struct XCodCloudTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
